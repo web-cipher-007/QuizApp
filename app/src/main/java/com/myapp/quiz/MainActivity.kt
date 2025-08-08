f157b86fdc4c3cfb7d3761e76cb2ae685f25436c
Sandesh Poudel <079bct074@ioepc.edu.np>
2025-08-08 20:13:53 +0545

    forces Dark Mode (for now)

--- app/src/main/java/com/myapp/quiz/MainActivity.kt
+++ app/src/main/java/com/myapp/quiz/MainActivity.kt
@@ -9,6 +9,7 @@ import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.ProgressBar
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.view.WindowInsetsCompat
 
 
@@ -16,6 +17,10 @@ import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        // Forces Light Mode
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
